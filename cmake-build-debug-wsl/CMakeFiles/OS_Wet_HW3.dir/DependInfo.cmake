65ee4f4f1176c5fe1f0734922f18c503ba637732
AnElectricalEngineer <47844685+AnElectricalEngineer@users.noreply.github.com>
2021-06-14 22:11:25 +0300

    remove unnecessary cmake-debug folder

--- cmake-build-debug-wsl/CMakeFiles/OS_Wet_HW3.dir/DependInfo.cmake
+++ cmake-build-debug-wsl/CMakeFiles/OS_Wet_HW3.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "D:/Technion/Operating Systems Wet Homework/OS-Wet-HW3/main.cpp" "D:/Technion/Operating Systems Wet Homework/OS-Wet-HW3/cmake-build-debug/CMakeFiles/OS_Wet_HW3.dir/main.cpp.obj"
+  "/mnt/d/Technion/Operating Systems Wet Homework/OS-Wet-HW3/main.cpp" "/mnt/d/Technion/Operating Systems Wet Homework/OS-Wet-HW3/cmake-build-debug-wsl/CMakeFiles/OS_Wet_HW3.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cmake-build-debug-wsl/CMakeFiles/OS_Wet_HW3.dir/cmake_clean.cmake
+++ cmake-build-debug-wsl/CMakeFiles/OS_Wet_HW3.dir/cmake_clean.cmake
@@ -1,9 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/OS_Wet_HW3.dir/main.cpp.obj"
-  "OS_Wet_HW3.exe"
-  "OS_Wet_HW3.exe.manifest"
+  "CMakeFiles/OS_Wet_HW3.dir/main.cpp.o"
+  "OS_Wet_HW3"
   "OS_Wet_HW3.pdb"
-  "libOS_Wet_HW3.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
