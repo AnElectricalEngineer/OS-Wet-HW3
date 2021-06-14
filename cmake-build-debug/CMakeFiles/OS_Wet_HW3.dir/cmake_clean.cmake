1fc510eb55e989af05579281e9b0b88db7493511
AnElectricalEngineer <47844685+AnElectricalEngineer@users.noreply.github.com>
2021-06-14 22:08:54 +0300

    Create Project

---
+++ cmake-build-debug/CMakeFiles/OS_Wet_HW3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_Wet_HW3.dir/main.cpp.obj"
+  "OS_Wet_HW3.exe"
+  "OS_Wet_HW3.exe.manifest"
+  "OS_Wet_HW3.pdb"
+  "libOS_Wet_HW3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS_Wet_HW3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
